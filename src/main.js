e9d57d390b451d054952fabb17a99870a4a8b9ef
Ilia Grigorev <ru.igsoft@gmail.com>
2026-01-09 17:25:22 +0300

    fix: Prevent hotkey from being recorded as a note

--- src/main.js
+++ src/main.js
@@ -59,6 +59,8 @@ const pressedNotes = new Set();
 let toggleCoolDown = false; // To prevent rapid toggling
 const demoGainNodes = new Set();
 
+let justToggledRecording = false;
+
 // --- State variables for recording ---
 let isRecording = false;
 let recordedNotes = [];
@@ -161,6 +163,11 @@ function pressKey(hitbox, pointerId, playAudio = true) {
     if (pointerId !== "demo") {
       pressedNotes.add(note);
       checkToggleCombination();
+    }
+
+    if (justToggledRecording) {
+      justToggledRecording = false;
+      return;
     }
 
     if (currentMode === "RECORDING" && !isRecording) {
@@ -341,6 +348,7 @@ function checkToggleCombination() {
     } else if (currentMode === "FREE_PLAY") {
       currentMode = "RECORDING";
       showToast("Recording...");
+      justToggledRecording = true;
       startRecording();
       console.log("Switched to RECORDING mode");
     } else if (currentMode === "RECORDING") {
